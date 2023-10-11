2661c85c47cef9da8afe8737015cb0b30d23f6be
Fio <leukert.fio@gmail.com>
2023-10-11 18:16:30 +0200

    improve choice edit layout

--- src/choice_edit.gd
+++ src/choice_edit.gd
@@ -3,30 +3,30 @@ extends HBoxContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$PageSelect.max_value = Pages.get_page_count() - 1
+	find_child("PageSelect").max_value = Pages.get_page_count() - 1
 
 
 func deserialize(data:Dictionary):
 	$LineEdit.text = data.get("choice_text")
-	$PageSelect.value = data.get("target_page")
+	find_child("PageSelect").value = data.get("target_page")
 	
 	update()
 
 func serialize():
 	return {
 		"choice_text": $LineEdit.text,
-		"target_page": $PageSelect.value
+		"target_page": find_child("PageSelect").value
 	}
 
 func _on_page_select_value_changed(value: float) -> void:
 	update()
 
 func update():
-	var default_target = int($PageSelect.value)
+	var default_target = int(find_child("PageSelect").value)
 	default_target = min(default_target, Pages.get_page_count() - 1)
 	
-	$PageKeyLabel.text = Pages.page_data.get(default_target).get("page_key")
-	$PageSelect.value = default_target
+	find_child("PageKeyLabel").text = Pages.page_data.get(default_target).get("page_key")
+	find_child("PageSelect").value = default_target
 
 
 func _on_delete_pressed() -> void:
