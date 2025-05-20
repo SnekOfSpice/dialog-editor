530663d520f3278ad8fba8bde2f49bc83527d9fc
Fio <leukert.fio@gmail.com>
2025-05-20 17:24:05 +0200

    alpha1

--- addons/diisis/editor/src/update_available.gd
+++ addons/diisis/editor/src/update_available.gd
@@ -18,11 +18,16 @@ func _on_http_request_request_completed(result: int, response_code: int, headers
 	if result != HTTPRequest.RESULT_SUCCESS: return
 
 	var current_version: String = Engine.get_meta("DIISISPlugin").get_version()
+	var local_subversion:=""
+	if "-" in current_version:
+		local_subversion = current_version.split("-")[1]
+		current_version = current_version.split("-")[0]
 	while current_version.count(".") < 3:
 		current_version += ".0"
 	var version_value = ""
 	for part in current_version.split("."):
 		version_value += part.rpad(3, "0")
+	version_value = int(version_value)
 	# Work out the next version from the releases information on GitHub
 	var response = JSON.parse_string(body.get_string_from_utf8())
 	
@@ -41,12 +46,29 @@ func _on_http_request_request_completed(result: int, response_code: int, headers
 			continue
 		tags_actual.append(tag)
 	tags_actual = tags_actual.filter(func(tag):
+		var subversion:=""
+		if "-" in tag:
+			subversion = tag.split("-")[1]
+			tag = tag.split("-")[0]
 		while tag.count(".") < 3:
 			tag += ".0"
 		var tag_value = ""
 		for part in tag.split("."):
 			tag_value += part.rpad(3, "0")
-		return int(tag_value) > int(version_value)
+		
+		tag_value = int(tag_value)
+		
+		if version_value == tag_value:
+			# subversion is not some halfbaked shit
+			if not local_subversion.is_empty() and subversion.is_empty():
+				return true
+			
+			if not subversion.is_empty() and not local_subversion.is_empty():
+				var subversion_value = subversion.trim_prefix("alpha")
+				var local_subversion_value = local_subversion.trim_prefix("alpha")
+				return int(subversion_value) > int(local_subversion_value)
+		
+		return tag_value > version_value
 		)
 	if tags_actual.size() > 0:
 		var newest_version : String = tags_actual[0]
