16c3a7c456e89875b05d914b6412ae507f3ab04f
Fio <leukert.fio@gmail.com>
2025-10-02 08:50:06 +0200

    add helpful dropdown texts :3

--- addons/diisis/editor/src/windows/dropdowns_container.gd
+++ addons/diisis/editor/src/windows/dropdowns_container.gd
@@ -10,17 +10,20 @@ func init():
 	working_memory_titles = Pages.dropdown_titles.duplicate(true)
 	
 	
-	for t in find_child("DropdownsContainer").get_children():
+	for t in %DropdownsContainer.get_children():
 		t.queue_free()
 	
 	for t in working_memory_titles:
 		var item = preload("res://addons/diisis/editor/src/dropdown_item.tscn").instantiate()
 		item.init(t)
-		find_child("DropdownsContainer").add_child(item)
+		%DropdownsContainer.add_child(item)
 		item.set_list_size(Pages.editor.get_window().size * 0.25)
-	
 	_on_clear_search_button_pressed()
 	Pages.apply_font_size_overrides(self)
+	
+	var size_before = (get_parent() as Window).size
+	await RenderingServer.frame_post_draw
+	(get_parent() as Window).size = size_before
 
 func fill_code_edit(tab_title: String):
 	var s = ""
@@ -32,17 +35,23 @@ func fill_code_edit(tab_title: String):
 
 
 func fill_dialog_argument_checkboxes():
-	for c in find_child("Dialog").get_children():
+	for c in %Dialog.get_children():
 		c.queue_free()
 	
 	var items := []
 	for dd_title : String in Pages.dropdown_titles:
 		var cb = preload("res://addons/diisis/editor/src/dialog_argument_dropdown_item.tscn").instantiate()
-		find_child("Dialog").add_child(cb)
+		%Dialog.add_child(cb)
 		cb.init(dd_title)
 		cb.argument_pressed.connect(toggle_dropdown_dialog_argument)
 		cb.syntax_pressed.connect(set_dialog_syntax_dropdown)
 		items.append(cb)
+	
+	if items.is_empty():
+		add_notice_to_container(
+			%Dialog,
+			"Currently there are no dropdowns defined for this file. Head over to the [code]Definitions[/code] tab to declare them.\n\nYou can use this tab to declare any dropdown as speaker or dialog argument (using the [code]{|}[/code] syntax in text lines.)"
+		)
 	
 	for item in items:
 		item.set_syntax_button_group(load("res://addons/diisis/editor/src/dialog_argument_syntax_button_group.tres"))
@@ -70,10 +79,10 @@ func _on_clear_search_button_pressed() -> void:
 
 func _on_search_line_edit_text_changed(new_text: String) -> void:
 	if new_text.is_empty():
-		for child in find_child("DropdownsContainer").get_children():
+		for child in %DropdownsContainer.get_children():
 			child.visible = true
 		return
-	for child in find_child("DropdownsContainer").get_children():
+	for child in %DropdownsContainer.get_children():
 		var filter:String
 		var search_term:String
 		if new_text.contains(":"):
@@ -97,15 +106,17 @@ func create_new_dropdown() -> void:
 	Pages.dropdown_titles.append(dd_title)
 	var item = preload("res://addons/diisis/editor/src/dropdown_item.tscn").instantiate()
 	item.init(dd_title)
-	find_child("DropdownsContainer").add_child(item)
+	%DropdownsContainer.add_child(item)
 	item.set_list_size(Pages.editor.get_window().size * 0.25)
 	find_child("CreateDDNameTextEdit").text = ""
 	Pages.apply_font_size_overrides(self)
 
 
 func _on_dropdowns_container_resized() -> void:
-	for child in find_child("DropdownsContainer").get_children():
+	for child in %DropdownsContainer.get_children():
 		child.set_list_size(Pages.editor.get_window().size * 0.25)
+	
+	%NoDropdownsNotice.visible = %DropdownsContainer.get_child_count() == 0
 
 
 func _on_create_dd_name_text_edit_text_submitted(_new_text: String) -> void:
@@ -114,8 +125,23 @@ func _on_create_dd_name_text_edit_text_submitted(_new_text: String) -> void:
 
 
 func _on_definitions_visibility_changed() -> void:
-	if not find_child("Definitions").visible:
+	if not %Definitions.visible:
 		return
 	
-	for child in find_child("DropdownsContainer").get_children():
+	for child in %DropdownsContainer.get_children():
 		child.update_speaker_label()
+	
+	%NoDropdownsNotice.visible = %DropdownsContainer.get_child_count() == 0
+
+func add_notice_to_container(container:Control, text:String):
+	var label = RichTextLabel.new()
+	label.bbcode_enabled = true
+	label.fit_content = true
+	container.add_child(label)
+	label.text = text
+
+
+func _on_tab_container_tab_changed(_tab: int) -> void:
+	var size_before = (get_parent() as Window).size
+	await RenderingServer.frame_post_draw
+	(get_parent() as Window).size = size_before
